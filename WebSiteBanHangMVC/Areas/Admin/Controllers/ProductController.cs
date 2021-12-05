5e50e8dfc3d1c1efcc7c48aefd8e294e65b5ed23
Lê Thế Long <78224213+thelong2212@users.noreply.github.com>
2021-12-05 15:11:16 +0700

    upadte

--- WebSiteBanHangMVC/Areas/Admin/Controllers/ProductController.cs
+++ WebSiteBanHangMVC/Areas/Admin/Controllers/ProductController.cs
@@ -1,45 +1,43 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebSiteBanHangMVC.DAO;
 using WebSiteBanHangMVC.Models;
-using WebSiteBanHangMVC.Utils;
 
 namespace WebSiteBanHangMVC.Areas.Admin.Controllers
 {
-    public class KhachHangController : BaseController
+    public class ProductController : BaseController
     {
-        // GET: Admin/KhachHang
+        // GET: Admin/Product
         public ActionResult Index(string searchString, int page = 1, int pageSize = 10)
         {
-            var dao = new KhachHangDAO();
-            var model = dao.ListAllpaging(searchString, page, pageSize);
-
-            ViewBag.SearchString = searchString;
-            return View(model);
+            var dao = new SanPhamDAO();
+            var mode = dao.ListAllpaging(searchString, page, pageSize);
+            ViewBag.Keyword = searchString;
+            return View(mode);
         }
 
-        // GET: Admin/KhachHang/Details/5
+        // GET: Admin/Product/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: Admin/KhachHang/Create
+        // GET: Admin/Product/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Admin/KhachHang/Create
+        // POST: Admin/Product/Create
         [HttpPost]
-        public ActionResult Create(KhachHang khachHang)
+        public ActionResult Create(SanPham sanPham)
         {
-            var dao = new KhachHangDAO();
-            var model = dao.insertKhachHang(khachHang);
-            if (model>0)
+            var dao = new SanPhamDAO();
+            long id = dao.Create(sanPham);
+            if (id > 0)
             {
                 SetAlert("Sửa thông tin khách hàng thành công", "success");
                 return RedirectToAction("Index", "KhachHang");
@@ -48,22 +46,23 @@ namespace WebSiteBanHangMVC.Areas.Admin.Controllers
             {
                 ModelState.AddModelError("", "Cập nhật thông tin khách hàng không thành công");
             }
-            return View(khachHang);
+            return View(sanPham);
         }
 
-        // GET: Admin/KhachHang/Edit/5
+        // GET: Admin/Product/Edit/5
+        [HttpGet]
         public ActionResult Edit(int id)
         {
-            var khacHang = new KhachHangDAO().ViewDetail(id);
-            return View(khacHang);
+            var sanPham = new SanPhamDAO().ViewDetail(id);
+            return View(sanPham);
         }
 
-        // POST: Admin/KhachHang/Edit/5
+        // POST: Admin/Product/Edit/5
         [HttpPost]
-        public ActionResult Edit(KhachHang khachHang)
+        public ActionResult Edit(SanPham sanPham)
         {
-            var dao = new KhachHangDAO();
-            var model = dao.Update(khachHang);
+            var dao = new SanPhamDAO();
+            var model = dao.Update(sanPham);
             if (model)
             {
                 SetAlert("Sửa thông tin khách hàng thành công", "success");
@@ -76,11 +75,11 @@ namespace WebSiteBanHangMVC.Areas.Admin.Controllers
             return View("Index");
         }
 
-        // GET: Admin/KhachHang/Delete/5
+        // GET: Admin/Product/Delete/5
         [HttpDelete]
         public ActionResult Delete(int id)
         {
-            new KhachHangDAO().Delete(id);
+            new SanPhamDAO().Delete(id);
             return RedirectToAction("Index");
         }
 
